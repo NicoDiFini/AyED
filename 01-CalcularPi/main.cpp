c67eb501e595004690674577d5eeb6bec1ef2857
Nicolás Di Fini <ndifini@frba.utn.edu.ar>
2023-10-10 19:44:05 -0300

    main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-double calcularPi(double iteraciones) 
+double calcularPi() 
 {
   double numerador = 4;
   double denominador = 1; 
@@ -10,22 +11,23 @@ double calcularPi(double iteraciones)
   int i = 0;
   int operador = 1;
 
-    for (; i < iteraciones; i++) 
-    {
-        pi += operador * (numerador / denominador);
-        denominador += 2;
-        operador *= -1;
-    }
+  while (abs(pi - 3.141592) > 0.0000001) 
+  {
+    pi += operador * (numerador / denominador);
+    denominador += 2;
+    operador *= -1;
+    i++;
+  }
 
-    return pi;
+  cout << "Se necesitaron " << i << " iteraciones para alcanzar la precision deseada." << endl;
+  return pi;
 }
 
 int main() 
 {
-    double iteraciones = 1000000;
-    double pi = calcularPi(iteraciones);
-    cout.precision (7);  
-    cout << "El valor aproximado de pi es: " << pi << endl;
+  double pi = calcularPi();
+  cout.precision(7);
+  cout << "El valor aproximado de pi es: " << pi << endl;
 
-    return 0;
+  return 0;
 }
